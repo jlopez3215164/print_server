5803e439a982d861445706011788003d114d71d8
JOSE <jlopez.xetux@gmail.com>
2022-04-14 14:25:01 -0400

    Ajuste

--- print-server/src/com/pos/print/server/threads/PrintFiscalThread.java
+++ print-server/src/com/pos/print/server/threads/PrintFiscalThread.java
@@ -1,37 +1,22 @@
 package com.pos.print.server.threads;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.pos.print.escposjava.PrinterService;
-import com.pos.print.escposjava.print.NetworkPrinter;
-import com.pos.print.escposjava.print.Printer;
-import com.pos.print.server.entity.DeviceEntity;
-import com.pos.print.server.entity.PrintDataEntity;
-import com.pos.print.server.entity.PrinterInstructionEntity;
-import com.pos.print.server.entity.TextInstructionEntity;
-import com.pos.print.server.exception.XErrorFatalPrintSeverException;
-import com.pos.print.server.exception.XErrorPrintServerConexion;
 import com.pos.print.server.servelt.PoolConfigLoadServlet;
+import com.printer.database.DriverManagerMail;
+import com.printer.exception.XDatabaseConexionException;
 
 public class PrintFiscalThread extends Thread {
 	private Logger log = LoggerFactory.getLogger(PrintFiscalThread.class);
-	
+
 	public PrintFiscalThread() {
-			
+
 	}
 
 	@Override
@@ -40,7 +25,40 @@ public class PrintFiscalThread extends Thread {
 			while (true) {
 				log.debug("Printer daemon Fiscal...");
 				
-				
+				Connection con = null;
+				PreparedStatement pstm = null;
+				ResultSet rs = null;
+				try{
+					con = DriverManagerMail.getDataSource().getConnection();
+					pstm = con.prepareStatement("select * from customer_order where is_print_fiscal = 1");
+		        	rs = pstm.executeQuery();
+		        	while(rs.next()) {
+		        		log.debug("Imprimiendo la factura...");
+		        		pstm = con.prepareStatement("update customer_order set is_print_fiscal = 2 where order_id = ?");
+		        		pstm.setInt(1, rs.getInt("order_id"));
+			        	pstm.execute();	
+		        	}
+					
+				}catch (XDatabaseConexionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        catch (Exception e)
+		        {
+		            e.printStackTrace();
+		        }finally{
+					try {
+						if(con != null)
+							con.close();
+						if(pstm != null)
+							pstm.close();
+						if(rs != null)
+							rs.close();
+		            } catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} 
+		        }
 				Thread.sleep(PoolConfigLoadServlet.THEARD_INTERVAL);
 			}
 		} catch (Throwable e) {
